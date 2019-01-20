d28b2b832f7aa65e46a9bef8d396b573fd4e3d70
Mate Papp <matepapp@icloud.com>
2019-01-20 18:13:21 +0100

    Create ecample slides and add image

--- deck.js
+++ deck.js
@@ -2,6 +2,7 @@ import welcome from './slides/00-welcome.mdx'
 import mainConcepts from './slides/01-main-concepts.mdx'
 import buildingBlocks from './slides/02-building-blocks.mdx'
 import rest from './slides/03-rest.mdx'
+import practicalExample from './slides/04-practical-example.mdx'
 
 export {
   components
@@ -17,5 +18,6 @@ export default [
   ...welcome,
   ...mainConcepts,
   ...buildingBlocks,
-  ...rest
+  ...rest,
+  ...practicalExample
 ]
