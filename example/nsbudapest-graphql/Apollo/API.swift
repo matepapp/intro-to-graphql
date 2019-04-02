655ae605fb7c583a376ef29d9eaaa7728d9b3e0d
Mate Papp <matepapp@icloud.com>
2019-04-02 21:47:04 +0200

    Designed profile tab

--- example/nsbudapest-graphql/Apollo/API.swift
+++ example/nsbudapest-graphql/Apollo/API.swift
@@ -4,7 +4,7 @@ import Apollo
 
 public final class ProfileQuery: GraphQLQuery {
   public let operationDefinition =
-    "query Profile {\n  viewer {\n    __typename\n    name\n    email\n    company\n    location\n    avatarUrl\n  }\n}"
+    "query Profile {\n  viewer {\n    __typename\n    login\n    avatarUrl\n    name\n    status {\n      __typename\n      emoji\n      message\n    }\n    bio\n    company\n    email\n    location\n    repositories {\n      __typename\n      totalCount\n    }\n  }\n}"
 
   public init() {
   }
@@ -41,11 +41,15 @@ public final class ProfileQuery: GraphQLQuery {
 
       public static let selections: [GraphQLSelection] = [
         GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-        GraphQLField("name", type: .scalar(String.self)),
-        GraphQLField("email", type: .nonNull(.scalar(String.self))),
-        GraphQLField("company", type: .scalar(String.self)),
-        GraphQLField("location", type: .scalar(String.self)),
+        GraphQLField("login", type: .nonNull(.scalar(String.self))),
         GraphQLField("avatarUrl", type: .nonNull(.scalar(String.self))),
+        GraphQLField("name", type: .scalar(String.self)),
+        GraphQLField("status", type: .object(Status.selections)),
+        GraphQLField("bio", type: .scalar(String.self)),
+        GraphQLField("company", type: .scalar(String.self)),
+        GraphQLField("email", type: .nonNull(.scalar(String.self))),
+        GraphQLField("location", type: .scalar(String.self)),
+        GraphQLField("repositories", type: .nonNull(.object(Repository.selections))),
       ]
 
       public private(set) var resultMap: ResultMap
@@ -54,8 +58,8 @@ public final class ProfileQuery: GraphQLQuery {
         self.resultMap = unsafeResultMap
       }
 
-      public init(name: String? = nil, email: String, company: String? = nil, location: String? = nil, avatarUrl: String) {
-        self.init(unsafeResultMap: ["__typename": "User", "name": name, "email": email, "company": company, "location": location, "avatarUrl": avatarUrl])
+      public init(login: String, avatarUrl: String, name: String? = nil, status: Status? = nil, bio: String? = nil, company: String? = nil, email: String, location: String? = nil, repositories: Repository) {
+        self.init(unsafeResultMap: ["__typename": "User", "login": login, "avatarUrl": avatarUrl, "name": name, "status": status.flatMap { (value: Status) -> ResultMap in value.resultMap }, "bio": bio, "company": company, "email": email, "location": location, "repositories": repositories.resultMap])
       }
 
       public var __typename: String {
@@ -64,6 +68,26 @@ public final class ProfileQuery: GraphQLQuery {
         }
         set {
           resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      /// The username used to login.
+      public var login: String {
+        get {
+          return resultMap["login"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "login")
+        }
+      }
+
+      /// A URL pointing to the user's public avatar.
+      public var avatarUrl: String {
+        get {
+          return resultMap["avatarUrl"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "avatarUrl")
         }
       }
 
@@ -77,13 +101,23 @@ public final class ProfileQuery: GraphQLQuery {
         }
       }
 
-      /// The user's publicly visible profile email.
-      public var email: String {
+      /// The user's description of what they're currently doing.
+      public var status: Status? {
         get {
-          return resultMap["email"]! as! String
+          return (resultMap["status"] as? ResultMap).flatMap { Status(unsafeResultMap: $0) }
         }
         set {
-          resultMap.updateValue(newValue, forKey: "email")
+          resultMap.updateValue(newValue?.resultMap, forKey: "status")
+        }
+      }
+
+      /// The user's public profile bio.
+      public var bio: String? {
+        get {
+          return resultMap["bio"] as? String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "bio")
         }
       }
 
@@ -97,6 +131,16 @@ public final class ProfileQuery: GraphQLQuery {
         }
       }
 
+      /// The user's publicly visible profile email.
+      public var email: String {
+        get {
+          return resultMap["email"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "email")
+        }
+      }
+
       /// The user's public profile location.
       public var location: String? {
         get {
@@ -107,13 +151,100 @@ public final class ProfileQuery: GraphQLQuery {
         }
       }
 
-      /// A URL pointing to the user's public avatar.
-      public var avatarUrl: String {
+      /// A list of repositories that the user owns.
+      public var repositories: Repository {
         get {
-          return resultMap["avatarUrl"]! as! String
+          return Repository(unsafeResultMap: resultMap["repositories"]! as! ResultMap)
         }
         set {
-          resultMap.updateValue(newValue, forKey: "avatarUrl")
+          resultMap.updateValue(newValue.resultMap, forKey: "repositories")
+        }
+      }
+
+      public struct Status: GraphQLSelectionSet {
+        public static let possibleTypes = ["UserStatus"]
+
+        public static let selections: [GraphQLSelection] = [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("emoji", type: .scalar(String.self)),
+          GraphQLField("message", type: .scalar(String.self)),
+        ]
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(emoji: String? = nil, message: String? = nil) {
+          self.init(unsafeResultMap: ["__typename": "UserStatus", "emoji": emoji, "message": message])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        /// An emoji summarizing the user's status.
+        public var emoji: String? {
+          get {
+            return resultMap["emoji"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "emoji")
+          }
+        }
+
+        /// A brief message describing what the user is doing.
+        public var message: String? {
+          get {
+            return resultMap["message"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "message")
+          }
+        }
+      }
+
+      public struct Repository: GraphQLSelectionSet {
+        public static let possibleTypes = ["RepositoryConnection"]
+
+        public static let selections: [GraphQLSelection] = [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("totalCount", type: .nonNull(.scalar(Int.self))),
+        ]
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(totalCount: Int) {
+          self.init(unsafeResultMap: ["__typename": "RepositoryConnection", "totalCount": totalCount])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        /// Identifies the total count of items in the connection.
+        public var totalCount: Int {
+          get {
+            return resultMap["totalCount"]! as! Int
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "totalCount")
+          }
         }
       }
     }
--- example/nsbudapest-graphql/View/Profile/ProfileViewController.swift
+++ example/nsbudapest-graphql/View/Profile/ProfileViewController.swift
@@ -7,29 +7,50 @@
 //
 
 import UIKit
+import Emoji
+import SkeletonView
 
 class ProfileViewController: UIViewController {
     @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
-    @IBOutlet private weak var emailLabel: UILabel!
+    @IBOutlet private weak var statusLabel: UILabel!
+    @IBOutlet private weak var bioLabel: UILabel!
     @IBOutlet private weak var companyLabel: UILabel!
+    @IBOutlet private weak var emailLabel: UILabel!
     @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var totalRepositories: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.showAnimatedSkeleton()
 
         Apollo.client.fetch(query: ProfileQuery()) { [unowned self] (result, error) in
             guard error == nil, let viewer = result?.data?.viewer else { return }
+            self.view.hideSkeleton()
             self.updateLabels(with: viewer)
         }
     }
 
     private func updateLabels(with viewer: ProfileQuery.Data.Viewer) {
+        title = viewer.login
+        avatarImageView.rounded.downloaded(from: viewer.avatarUrl, contentMode: .scaleAspectFill)
         nameLabel.text = viewer.name
-        emailLabel.text = viewer.email
+        statusLabel.text = constructStatusLabelText(from: viewer.status)
+        bioLabel.text = viewer.bio
         companyLabel.text = viewer.company
+        emailLabel.text = viewer.email
         locationLabel.text = viewer.location
-        avatarImageView.downloaded(from: viewer.avatarUrl, contentMode: .scaleAspectFill)
+        totalRepositories.text = "\(viewer.repositories.totalCount) repositories"
+    }
+}
+
+extension ProfileViewController {
+    private func constructStatusLabelText(from status: ProfileQuery.Data.Viewer.Status?) -> String? {
+        guard let statusEmoji = status?.emoji, let statusMessage = status?.message else {
+            return nil
+        }
+
+        return "\(statusEmoji.emojiUnescapedString) - \(statusMessage)"
     }
 }
 
